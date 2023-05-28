abe4e5c2ffb6b63acaaab1e5066e22fa9d972f34
abedinforhan <abedinforhan@gmail.com>
2023-05-28 06:27:37 +0600

    testing

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,9 @@
-import dotenv from "dotenv";
-import path from "path";
+import dotenv from 'dotenv'
+import path from 'path'
 
-dotenv.config({ path: path.join(process.cwd(), ".env") });
+dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
-};
+}
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import mongoose from "mongoose";
-import config from "./config/index";
-import app from "./app";
+import mongoose from 'mongoose'
+import config from './config/index'
+import app from './app'
 
 async function boostrap() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log(`🛢   Database is connected successfully`);
+    await mongoose.connect(config.database_url as string)
+    console.log(`🛢   Database is connected successfully`)
 
     app.listen(config.port, () => {
-      console.log(`Application  listening on port ${config.port}`);
-    });
+      console.log(`Application  listening on port ${config.port}`)
+    })
   } catch (err) {
-    console.log("Failed to connect database", err);
+    console.log('Failed to connect database', err)
   }
 }
 
-boostrap();
+boostrap()
