1c55fb3e4af5d1a3d50b2948959ba7e5311109ff
abedinforhan <abedinforhan@gmail.com>
2023-06-06 13:28:34 +0600

    academic semester module created & optimized codebase

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -3,6 +3,7 @@ import { RequestHandler } from 'express-serve-static-core';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
+import { IUser } from './user.interface';
 import { UserService } from './user.service';
 
 const createUser: RequestHandler = catchAsync(
@@ -10,13 +11,13 @@ const createUser: RequestHandler = catchAsync(
     const { user } = req.body;
     const result = await UserService.createUser(user);
 
-    next();
-    sendResponse(res, {
+    sendResponse<IUser>(res, {
       statusCode: httpStatus.OK,
       success: true,
       message: 'user created successfully!',
       data: result,
     });
+    next();
   }
 );
 
